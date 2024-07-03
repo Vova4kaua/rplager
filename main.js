b4d937c5ef1735ab6c7747113a34804d20a97cac
Vova4kaua <167625535+Vova4kaua@users.noreply.github.com>
2024-07-03 21:13:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-if (document.getElementById("rooms").value = 2) {
-    document.getElementById("room").textContent = "11 room"
+function roomss() {
+    var selectedValue = document.getElementById("rooms").value;
+
+    if (selectedValue == 2) {
+        document.getElementById("room").innerText = "11 Комната";
+    }
+    if (selectedValue == 3) {
+        document.getElementById("room").innerText = "12 Комната";
+        document.getElementById("item-9").innerText = "@baby_lol24_02"
+        document.getElementById("item-5").innerText = "@syperkolt"
+    }
+    if (selectedValue == 4) {
+        document.getElementById("room").innerText = "14 Комната";
+    }
+    if (selectedValue == 5) {
+        document.getElementById("room").innerText = "15 Комната";
+        document.getElementById("item-9").innerText = "@Gggbjf0"
+    }
 }
